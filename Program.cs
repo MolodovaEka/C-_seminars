6f4285fe699985b265be2c6907ebe1dcaf7bff73
Eka_Eka <ekamolodova@gmail.com>
2022-12-20 23:42:18 -0800

    add sum

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");//
+
+/*Console.WriteLine ("Type in your username ");
+string username = Console.ReadLine();
+Console.Write("Hello, ");
+Console.Write(username);*/
+
+
